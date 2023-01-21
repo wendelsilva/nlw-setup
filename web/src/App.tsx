6a4664fb98132cb798e1c1d59073b638c7611b83
wendelsilva <wendel245.ws@gmail.com>
2023-01-21 17:51:06 -0300

    connected api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,8 @@ import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
 import './styles/global.css';
 
+import './lib/dayjs'
+
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,6 +1,10 @@
 import { generateDatesfromBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
+import { useEffect, useState } from 'react' 
+import { api } from "../lib/axios"
+import dayjs from "dayjs"
+
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 const summaryDates = generateDatesfromBeginning()
@@ -8,9 +12,21 @@ const summaryDates = generateDatesfromBeginning()
 const minimumSummaryDatesSize = 18 * 7
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
-console.log(summaryDates);
+type Summary = {
+    id: string,
+    date: string,
+    amount: number,
+    completed: number
+}[]
 
 export function SummaryTable () {
+    const [summary, setSummary] = useState<Summary>([])
+
+    useEffect(() => {
+        api.get('/summary').then(response => setSummary(response.data))
+    }, [])
+
+
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -25,11 +41,16 @@ export function SummaryTable () {
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(date => {
+                    const dayInSummary = summary.find(day => {
+                        return dayjs(date).isSame(day.date, 'day')
+                    })
+
                     return (
                         <HabitDay 
                             key={date.toString()}
-                            amount={5} 
-                            completed={Math.round(Math.random() * 5)} 
+                            date={date}
+                            amount={dayInSummary?.amount} 
+                            completed={dayInSummary?.completed} 
                         />
                     )
                 })}
